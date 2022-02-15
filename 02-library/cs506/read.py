dbe8d8010147a4319371a762099e59cb864d0e62
Samantha Puterman <Samanpg@bu.edu>
2022-02-14 20:08:19 -0500

    implemented read and sim.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,12 @@
+import pandas as pd 
+
+
 def read_csv(csv_file_path):
+    csv = pd.read_csv(csv_file_path, header=None)
+    list_of_list = [list(row) for row in csv.values]
+    return list_of_list 
+   # return pd.read_csv(csv_file_path, header=None).to_numpy()
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,3 +1,9 @@
+import math 
+import numpy as np 
+import scipy 
+from scipy import spatial
+from scipy.spatial import distance
+
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +11,18 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    return sum(abs(val1-val2) for val1, val2 in zip(x,y))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if (len(x)==0 or len(y)==0):
+        return "length cannot equal 0"
+    intersection = len(list(set(x).intersection(set(y))))
+    union = set(x).union(set(y))
+    return 1 - float(intersection) / len(union)
+
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    return 1 - spatial.distance.cosine(x, y)
+
 
 # Feel free to add more
